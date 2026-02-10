9ad1ef474bfd1ed25b4e2066d7b275a8077caa27
rahulrana-jpg <rr6793112@gmail.com>
2026-02-10 10:16:33 +0545

    hello

---
+++ main.js
@@ -0,0 +1,38 @@
+// let a=10
+// let b=5
+// let addition=a+b
+// let subtraction=a-b
+// let multiplication=a*b
+// let division=a/b
+// let modulus=a%b
+// let exponentiation=a**b
+// console.log("addition: "+addition)
+// console.log("subtraction: "+subtraction)
+// console.log("multiplication: "+multiplication)
+// console.log("division: "+division)
+// console.log("modulus: "+modulus)
+// console.log("exponentiation: "+exponentiation)
+
+
+// let a=10
+// let b=5
+// let c=15
+// let d=20
+
+// console.log(a+=1)
+// console.log(b-=1)
+// console.log(c*=2)
+// console.log(d/=2)
+
+// let a=10
+// let b=9
+// console.log(a>=b)
+
+// let a=10
+// let b="10"
+
+// console.log(a===b)
+
+let  celsius= 10
+let fahrenheit= (celsius*9/5)+32
+console.log("temperature in fahrenheit: "+fahrenheit)

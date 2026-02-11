35e8593b11b8075b637be39cb5184f3e1d51e970
rahulrana-jpg <rr6793112@gmail.com>
2026-02-11 09:14:23 +0545

    hello world

---
+++ hello.js
@@ -0,0 +1,11 @@
+//Day3: conditional statements in javascript
+let a=20
+let b=10
+
+//if statement
+if(a>b){
+    console.log("a is greater than b")
+}
+else{
+    console.log("b is greater than a")
+}
--- main.js
+++ main.js
@@ -1,38 +1,50 @@
-// let a=10
-// let b=5
-// let addition=a+b
-// let subtraction=a-b
-// let multiplication=a*b
-// let division=a/b
-// let modulus=a%b
-// let exponentiation=a**b
-// console.log("addition: "+addition)
-// console.log("subtraction: "+subtraction)
-// console.log("multiplication: "+multiplication)
-// console.log("division: "+division)
-// console.log("modulus: "+modulus)
-// console.log("exponentiation: "+exponentiation)
+// // let a=10
+// // let b=5
+// // let addition=a+b
+// // let subtraction=a-b
+// // let multiplication=a*b
+// // let division=a/b
+// // let modulus=a%b
+// // let exponentiation=a**b
+// // console.log("addition: "+addition)
+// // console.log("subtraction: "+subtraction)
+// // console.log("multiplication: "+multiplication)
+// // console.log("division: "+division)
+// // console.log("modulus: "+modulus)
+// // console.log("exponentiation: "+exponentiation)
 
 
-// let a=10
-// let b=5
-// let c=15
-// let d=20
+// // let a=10
+// // let b=5
+// // let c=15
+// // let d=20
 
-// console.log(a+=1)
-// console.log(b-=1)
-// console.log(c*=2)
-// console.log(d/=2)
+// // console.log(a+=1)
+// // console.log(b-=1)
+// // console.log(c*=2)
+// // console.log(d/=2)
 
-// let a=10
-// let b=9
-// console.log(a>=b)
+// // let a=10
+// // let b=9
+// // console.log(a>=b)
 
-// let a=10
-// let b="10"
+// // let a=10
+// // let b="10"
 
-// console.log(a===b)
+// // console.log(a===b)
 
-let  celsius= 10
-let fahrenheit= (celsius*9/5)+32
-console.log("temperature in fahrenheit: "+fahrenheit)
+// // let  celsius= 10
+// // let fahrenheit= (celsius*9/5)+32
+// // console.log("temperature in fahrenheit: "+fahrenheit)
+
+// let weight = 10
+// let height =50
+// let BMI = weight/(height*height)
+// console.log=("the BMI is: " + BMI)
+
+
+let h=Number(prompt("enter the height"))
+let w= Number(prompt("enter the weight"))
+let BMI = w/(h*h)
+console.log("the BMI is: " + BMI)
+

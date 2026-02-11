40db788033a447929f6934c815839dc9ecc45fb8
rahulrana-jpg <rr6793112@gmail.com>
2026-02-11 09:21:44 +0545

    hello 2

--- hello.js
+++ hello.js
@@ -1,11 +1,21 @@
-//Day3: conditional statements in javascript
+// //Day3: conditional statements in javascript
 let a=20
 let b=10
+let c=15
 
-//if statement
-if(a>b){
-    console.log("a is greater than b")
+// //if statement
+// if(a>b){
+//     console.log("a is greater than b")
+// }
+// else{
+//     console.log("b is greater than a")
+// }
+if(a>b && a>c){
+    console.log("a is the greatest")
+}
+else if(b>c){
+    console.log("b is the greatest")
 }
 else{
-    console.log("b is greater than a")
+    console.log("c is the greatest")
 }
